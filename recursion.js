b26c7b2b3a29438d98b16c014889715e54dc215f
Dzmitry <35468938+DrDiman@users.noreply.github.com>
2018-10-28 19:37:53 +0300

    feat: feat: add function#3 "recursion" NOT implemented
    function "recursion" has bugs! =(

--- recursion.js
+++ recursion.js
@@ -1,5 +1,23 @@
-module.exports = function recursion(objTree) {
+module.exports = function recursion(arg) {
   const arrayTree = [];
-  // transformation into array
-  return arrayTree;
+
+  function myfunc(objTree) {
+    const val = objTree.value;
+    arrayTree.push([val]);
+
+    if (!(objTree.hasOwnProperty('left')) || !(objTree.hasOwnProperty('right'))) {
+      return arrayTree;
+    }
+
+    if (objTree.hasOwnProperty('left')) {
+      objTree = objTree.left;
+      return myfunc(objTree);
+    }
+
+    if (objTree.hasOwnProperty('right')) {
+      objTree = objTree.right;
+      return myfunc(objTree);
+    }
+  }
+  return myfunc(arg);
 };
